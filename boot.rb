c7532a64f4b34ff4b95c9ce6de81e7e7680d002a
Maik Brendler <maik.brendler@injixo.com>
2015-03-30 16:25:45 +0200

    Count error messages with redis.

--- boot.rb
+++ boot.rb
@@ -1,4 +1,6 @@
 require 'sneakers'
+require 'redis'
+require 'json'
 
 Sneakers.configure
 Sneakers.logger.level = Logger::INFO
@@ -7,7 +9,11 @@ class Processor
   include Sneakers::Worker
   from_queue :logs
 
+  REDIS = Redis.new
+
   def work(msg)
-    logger.info msg
+    err = JSON.parse(msg)
+    REDIS.incr("processor:#{err['error']}") if err['type'] == 'error'
+    ack!
   end
 end
