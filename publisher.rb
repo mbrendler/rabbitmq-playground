dbabf5f1ae2ebb0abdd9aab9038f04e3d91e54e6
Maik Brendler <maik.brendler@injixo.com>
2020-01-16 01:28:34 +0100

    Allow to make queue durable

--- publisher.rb
+++ publisher.rb
@@ -22,12 +22,16 @@ end
 
 def parse_options
   queue_name = 'a_test_queue'
+  queue_options = { durable: true }
   message_properties = {}
   OptionParser.new do |opts|
     opts.banner = "Usage: #{$PROGRAM_NAME} [options] MESSAGE..."
     opts.separator('')
 
     opts.on('--queue QUEUE', 'Use queue') { |value| queue_name = value }
+    opts.on('--queue-no-durable', 'disable durable') do
+      queue_options[:durable] = false
+    end
 
     opts.separator('')
     opts.separator('Message Properties:')
@@ -38,15 +42,15 @@ def parse_options
       end
     end
   end.parse!
-  [queue_name, message_properties]
+  [queue_name, queue_options, message_properties]
 end
 
 def main
-  queue_name, message_properties = parse_options
+  queue_name, queue_options, message_properties = parse_options
   connection = Bunny.new
   connection.start
   channel = connection.create_channel
-  queue = channel.queue(queue_name)
+  queue = channel.queue(queue_name, queue_options)
   messages = ARGV.empty? ? [Time.now.iso8601] : ARGV
   messages.each do |message|
     puts "publish: #{message}"
