281d4641cf66748868478f4b2a83732eed556e4e
Maik Brendler <maik.brendler@injixo.com>
2020-03-02 19:48:48 +0100

    Add config

---
+++ config.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+EXCHANGE = 'playground.a-exchange'
+EXCHANGE_TYPE = :topic
+EXCHANGE_OPTIONS = { durable: false }.freeze
+
+ROUTING_KEY = 'playground.a-routing-key'
+
+QUEUE_NAME = 'a_test_queue'
+QUEUE_DURABLE = false
